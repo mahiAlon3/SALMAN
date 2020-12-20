109f3cd1716a468dc1096d1e3d181c3e92a5494c
mahiAlon3 <76397239+mahiAlon3@users.noreply.github.com>
2020-12-20 21:07:24 +0500

    Add files via upload

--- zeroboy.py
+++ zeroboy.py
@@ -49,16 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - Salman -OFFICAL------•◈•
-|__\~~) •||•SALMAN - Mahi---------------•◈•
-|__(-----\  •◈•------MAHI-ALONE--------•◈•
+|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
+|__\~~) •||•RANA - RAJPUT---------------•◈•
+|__(-----\  •◈•------BLACK-TIGER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
-Author Salman Mahi
-\033[1;96mInstagram Salman farooq
-\033[1;96mFacebook Salman Farooq
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Salmanmahi/Alone
+\033[1;91m=======================================
+\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
+\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
+\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -79,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92m Salman Farooq
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlow Salman Farooq
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Salman Farooq
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Salmanmahi/Alone
+\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = Mahi
-CorrectPassword = Mahi
+CorrectUsername = "rana"
+CorrectPassword = "rana"
 
 loop = 'true'
 while (loop == 'true'):
